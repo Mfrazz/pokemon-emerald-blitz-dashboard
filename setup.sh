85c6d81474cd42453dc65113bed294bec2acef13
Matt F <Matthewfrazzetta@gmail.com>
2026-01-17 03:20:35 -0500

    Add setup.sh to init submodule

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Initialize submodules (if any)
+git submodule update --init --recursive
+
+# List files in your submodule folder to confirm
+echo "Contents of pokemon-assets:"
+ls -R "Insert into Database/pokemon-assets"
